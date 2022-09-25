16d73b4fe54ed3ed12e7a6d62c83c306fecce0ad
Zachary Lin <z2x3c4v5bz@gmail.com>
2022-09-25 16:39:38 +0800

    modify ch13 code

--- ch13/hw13_01/hw13_01.py
+++ ch13/hw13_01/hw13_01.py
@@ -2,25 +2,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-urlstr = 'https://www.taiwanlottery.com.tw/index_new.aspx'
-responseObj = requests.get(urlstr)
-bs = BeautifulSoup(responseObj.text, 'html.parser')
 
-#print(bs.title.text)
+if __name__ == '__main__':
 
-data = bs.select('.contents_box02')
-link = data[2].find_all('div')
-print('\n本期大樂透開獎結果如下 :')
-print('號碼順序 :', end=' ')
-for n in range(9, len(link) - 1):
-    print(link[n].text, end=' ')
-print('\n特別號碼 :', end=' ')
-print(link[-1].text)
+    urlstr = 'https://www.taiwanlottery.com.tw/index_new.aspx'
+    responseObj = requests.get(urlstr)
+    bs = BeautifulSoup(responseObj.text, 'html.parser')
 
-input("Press Enter to continue . . .")
+    #print(bs.title.text)
+
+    data = bs.select('.contents_box02')
+    link = data[2].find_all('div')
+
+    print('\n本期大樂透開獎結果如下 :')
+    print('號碼順序 :', end=' ')
+
+    for n in range(9, len(link) - 1):
+        print(link[n].text, end=' ')
+
+    print('\n特別號碼 :', end=' ')
+    print(link[-1].text)
+
+    input("Press Enter to continue . . .")
 
 
-'''
+''' Output
 
 本期大樂透開獎結果如下 :
 號碼順序 : 01  05  08  11  35  36
