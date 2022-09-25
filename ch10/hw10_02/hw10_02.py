1cb50d6459b34e83f03376229fa9c299cd302d68
Zachary Lin <z2x3c4v5bz@gmail.com>
2022-09-25 16:24:31 +0800

    modify ch10 code

--- ch10/hw10_02/hw10_02.py
+++ ch10/hw10_02/hw10_02.py
@@ -1,35 +1,50 @@
 # hw10_02
 import os
 
-if os.path.exists('poem1.txt'):
-    p1 = open('poem1.txt', 'r', encoding = 'utf-8')
-else:
-    p1 = open('poem1.txt', 'w+', encoding = 'utf-8')
-    p1.write('山中雜詩\t\t吳均\n')
-    p1.write('山 際 見 來 煙 。\n')
-    p1.write('竹 中 窺 落 日 。\n')
-    p1.write('鳥 向 簷 上 飛 。\n')
-    p1.write('雲 從 窗 裏 出 。')
-if os.path.exists("poem2.txt"):
-    p2 = open('poem2.txt', 'r', encoding = 'utf-8')
-else:
-    p2 = open('poem2.txt', 'w+', encoding = 'utf-8')
-    p2.write('贈范曄詩\t\t陸凱\n')
-    p2.write('折 花 逢 驛 使 ，\n')
-    p2.write('寄 與 隴 頭 人 。\n')
-    p2.write('江 南 無 所 有 ，\n')
-    p2.write('聊 贈 一 枝 香 。')
 
-p1.seek(0)
-p2.seek(0)
-p3 = open('poem3.txt', 'w', encoding = 'utf-8')
+if __name__ == '__main__':
 
-for line in p1:
-    p3.write(line)
-p3.write('\n\n')
-for line in p2:
-    p3.write(line)
+    if os.path.exists('poem1.txt'):
 
-p1.close()
-p2.close()
-p3.close()
+        p1 = open('poem1.txt', 'r', encoding = 'utf-8')
+
+    else:
+
+        p1 = open('poem1.txt', 'w+', encoding = 'utf-8')
+
+        p1.write('山中雜詩\t\t吳均\n')
+        p1.write('山 際 見 來 煙 。\n')
+        p1.write('竹 中 窺 落 日 。\n')
+        p1.write('鳥 向 簷 上 飛 。\n')
+        p1.write('雲 從 窗 裏 出 。')
+
+    if os.path.exists("poem2.txt"):
+
+        p2 = open('poem2.txt', 'r', encoding = 'utf-8')
+
+    else:
+
+        p2 = open('poem2.txt', 'w+', encoding = 'utf-8')
+
+        p2.write('贈范曄詩\t\t陸凱\n')
+        p2.write('折 花 逢 驛 使 ，\n')
+        p2.write('寄 與 隴 頭 人 。\n')
+        p2.write('江 南 無 所 有 ，\n')
+        p2.write('聊 贈 一 枝 香 。')
+
+    p1.seek(0)
+    p2.seek(0)
+
+    p3 = open('poem3.txt', 'w', encoding = 'utf-8')
+
+    for line in p1:
+        p3.write(line)
+
+    p3.write('\n\n')
+    
+    for line in p2:
+        p3.write(line)
+
+    p1.close()
+    p2.close()
+    p3.close()
--- ch10/hw10_03/hw10_03.py
+++ ch10/hw10_03/hw10_03.py
@@ -1,34 +1,45 @@
 # hw10_03
 import os
 
-userpath = input('請輸入資料夾路徑 : ')
-userfile = userpath + input('請輸入檔案名稱 : ')
 
-if not os.path.exists(userpath):
-    os.mkdir(userpath)
-if not os.path.isfile(userfile):
-    uFile = open(userfile, 'w', encoding='utf-8')
-    uFile.write('姓名      國文  英語\n')
-    uFile.write('=' * 20)
-    uFile.write('\n')
+if __name__ == '__main__':
+
+    userpath = input('請輸入資料夾路徑 : ')
+    userfile = userpath + input('請輸入檔案名稱 : ')
+
+    if not os.path.exists(userpath):
+        os.mkdir(userpath)
+
+    if not os.path.isfile(userfile):
+
+        uFile = open(userfile, 'w', encoding='utf-8')
+
+        uFile.write('姓名      國文  英語\n')
+        uFile.write('=' * 20)
+        uFile.write('\n')
+
+        uFile.close()
+
+    uFile = open(userfile, 'a+', encoding='utf-8')
+
+    while True:
+
+        adddata = input('是否要輸入資料?(y/n) : ')
+
+        if adddata == 'n':
+
+            break
+
+        newdata = input('輸入學生姓名,國文,英語成績 : ')
+        newdata = newdata.split(',')
+        uFile.write('{0} {1}  {2}\n'.format(newdata[0].ljust(6), newdata[1].rjust(4), newdata[2].rjust(4)))
+
+    uFile.seek(0)
+    print(uFile.read())
     uFile.close()
 
-uFile = open(userfile, 'a+', encoding='utf-8')
 
-while True:
-    adddata = input('是否要輸入資料?(y/n) : ')
-    if adddata == 'n':
-        break
-    newdata = input('輸入學生姓名,國文,英語成績 : ')
-    newdata = newdata.split(',')
-    uFile.write('{0} {1}  {2}\n'.format(newdata[0].ljust(6), newdata[1].rjust(4), newdata[2].rjust(4)))
-
-uFile.seek(0)
-print(uFile.read())
-uFile.close()
-
-
-'''
+''' Output
 
 請輸入資料夾路徑 : C:\\Users\\Python\\Python基礎必修課(含MTA Python微軟國際認證模擬試題)\\Practices\\ch10\\hw10_03\\
 請輸入檔案名稱 : score.txt
