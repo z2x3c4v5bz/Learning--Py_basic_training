1f85b83d6780a65399f249a30a28a387b7565100
Zachary Lin <z2x3c4v5bz@gmail.com>
2022-09-25 16:29:39 +0800

    modify ch11 code

--- ch11/hw11_02/hw11_02.py
+++ ch11/hw11_02/hw11_02.py
@@ -1,13 +1,18 @@
 # hw11_02
 import matplotlib.pyplot as plt
 
-font = {'family' : 'DFKai-SB'}
-plt.rc('font', **font)
-listPercent = [25.2, 31.8, 43]
-listDep = ["商管", "資電", "設計"]
-listExplode = [0, 0, 0.1]
-plt.pie(listPercent, shadow=True, labels=listDep, explode=listExplode, autopct='%2.1f%%')
-plt.title('碁峰科大招生員額比例')
-plt.axis('equal')
-plt.legend()
-plt.show()
+
+if __name__ == '__main__':
+
+    font = {'family' : 'DFKai-SB'}
+    plt.rc('font', **font)
+
+    listPercent = [25.2, 31.8, 43]
+    listDep = ["商管", "資電", "設計"]
+    listExplode = [0, 0, 0.1]
+    
+    plt.pie(listPercent, shadow=True, labels=listDep, explode=listExplode, autopct='%2.1f%%')
+    plt.title('碁峰科大招生員額比例')
+    plt.axis('equal')
+    plt.legend()
+    plt.show()
